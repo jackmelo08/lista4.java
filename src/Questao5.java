fe8844538535eab251530e6bcafe404f31ede83f
jackmelo08 <143668270+jackmelo08@users.noreply.github.com>
2024-08-23 20:15:53 -0300

    Add files via upload

--- src/Questao5.java
+++ src/Questao5.java
@@ -3,23 +3,36 @@ import java.util.Scanner;
 public class Questao5 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Escreva o tamanho do vetor: ");
+
+        System.out.print("Escreva o tamanho do vetor: ");
         int n = s.nextInt();
         int[] vetor = new int[n];
 
-        System.out.println("Escreva " + n + " números inteiros:");
+        System.out.println("Escreva os elementos do vetor:");
         for (int i = 0; i < n; i++) {
             vetor[i] = s.nextInt();
         }
-        for
 
-            int contador = 0;
-            int nPrimo = 0;
-            for (int num : vetor) {
-                if (nPrimo ) {
-                    contador++;
-                }
-
+        int contPrimos = 0;
+        for (int i = 0; i < n; i++) {
+            if (isPrimo(vetor[i])) {
+                contPrimos++;
+            }
         }
+
+        System.out.println("O vetor tem quantos numeros primos: " + contPrimos);
+    }
+
+    public static boolean isPrimo(int num) {
+        if (num <= 1) return false;
+        if (num == 2) return true;
+        if (num % 2 == 0) return false;
+
+        for (int i = 3; i <= Math.sqrt(num); i += 2) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
